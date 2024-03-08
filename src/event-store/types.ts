d9f8a2d2298dfb930562dd573a5868ef8a99cd17
Jeel Patel <34994081+JeelPatel2000@users.noreply.github.com>
2024-03-09 00:17:11 +0800

    event store types

---
+++ src/event-store/types.ts
@@ -0,0 +1,26 @@
+interface Payload {
+  [key: string]: unknown,
+  eventType: string
+}
+
+export interface EventEnvelope {
+  streamId: string
+  version: number
+  eventType: Payload['eventType']
+  payload: Payload
+}
+
+export interface DurableEventEnvelope extends EventEnvelope {
+  position: number
+}
+
+export interface Stream {
+  streamId: string
+  expectedVersion: number
+  events: DurableEventEnvelope[]
+}
+
+export interface EventStore {
+  read: (streamId: string) => Promise<void>
+  append: (stream: Stream) => Promise<void>
+}
